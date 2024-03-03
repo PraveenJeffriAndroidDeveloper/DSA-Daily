8ca096712cece1f74f9b424393e9e6e267eb08d4
PraveenJeffri <praveenjeffri.gmail.com>
2024-03-03 11:54:56 +0530

    4 recursion problems done

---
+++ src/Recursion/Rec7.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Rec7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = factorial(3);
+        System.out.println(num);
+    }
+
+    public static int factorial(int n)
+    {
+        if(n==1) return 1;
+        return n * factorial(n-1);
+    }
+}
---
+++ src/Recursion/Rec9.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class Rec9 {
+    public static void main(String[] args) {
+
+    }
+    public int fib(int n) {
+        if(n==0)
+            return 0;
+        int first = 0;
+        int second = 1;
+        for(int i=2;i<=n;i++)
+        {
+            int temp = first ;
+            first = second ;
+            second = temp+second;
+        }
+
+        return second;
+
+    }
+}
